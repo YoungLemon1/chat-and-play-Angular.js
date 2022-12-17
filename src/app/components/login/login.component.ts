198071943f9682ff7ef1d4553f979c2a880e5c93
YoungLemon1 <110844957+YoungLemon1@users.noreply.github.com>
2022-12-17 19:46:58 +0200

    delete import

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import User from 'src/app/models/user.model';
 import UserService from 'src/app/services/user.service';
 
 @Component({
